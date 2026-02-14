b6c82e425ada0941114fbf713e9afa3bf3067fdd
ZhenyaKosovan <yevhen.kosovan@outlook.com>
2026-02-14 12:05:46 +0100

    Make testing of exact solution vs gradient descent more flexible

--- tests/testthat/test_against_reference.R
+++ tests/testthat/test_against_reference.R
@@ -1,55 +1,48 @@
 tol <- .03
 min.decrease <- 1e-6
 max.iter <- 1e6
-if (TRUE) {
-  test_that("synthdid point estimate agrees with the reference implementation", {
-    if (!requireNamespace("CVXR", quietly = TRUE)) {
-      stop("Package CVXR must be installed to run this test.")
-    }
-    data(california_prop99)
-    setup <- panel.matrices(california_prop99)
-    expect_equal(
-      c(synthdid_estimate(
-        setup$Y,
-        setup$N0,
-        setup$T0,
-        min.decrease = min.decrease,
-        max.iter = max.iter
-      )),
-      c(synthdid.reference(setup$Y, setup$N0, setup$T0)),
-      tolerance = tol
-    )
-  })
 
-  test_that("sc point estimate agrees with the reference implementation", {
-    if (!requireNamespace("CVXR", quietly = TRUE)) {
-      stop("Package CVXR must be installed to run this test.")
-    }
-    data(california_prop99)
-    setup <- panel.matrices(california_prop99)
-    expect_equal(
-      c(sc_estimate(
-        setup$Y,
-        setup$N0,
-        setup$T0,
-        min.decrease = min.decrease,
-        max.iter = max.iter
-      )),
-      c(sc.reference(setup$Y, setup$N0, setup$T0)),
-      tolerance = tol
-    )
-  })
+test_that("synthdid point estimate agrees with the reference implementation", {
+  skip_if_not_installed("CVXR")
+  data(california_prop99)
+  setup <- panel.matrices(california_prop99)
+  expect_equal(
+    c(synthdid_estimate(
+      setup$Y,
+      setup$N0,
+      setup$T0,
+      min.decrease = min.decrease,
+      max.iter = max.iter
+    )),
+    c(synthdid.reference(setup$Y, setup$N0, setup$T0)),
+    tolerance = tol
+  )
+})
 
-  test_that("did point estimate agrees with the reference implementation", {
-    if (!requireNamespace("CVXR", quietly = TRUE)) {
-      stop("Package CVXR must be installed to run this test.")
-    }
-    data(california_prop99)
-    setup <- panel.matrices(california_prop99)
-    expect_equal(
-      c(did_estimate(setup$Y, setup$N0, setup$T0)),
-      c(did.reference(setup$Y, setup$N0, setup$T0)),
-      tolerance = tol
-    )
-  })
-}
+test_that("sc point estimate agrees with the reference implementation", {
+  skip_if_not_installed("CVXR")
+  data(california_prop99)
+  setup <- panel.matrices(california_prop99)
+  expect_equal(
+    c(sc_estimate(
+      setup$Y,
+      setup$N0,
+      setup$T0,
+      min.decrease = min.decrease,
+      max.iter = max.iter
+    )),
+    c(sc.reference(setup$Y, setup$N0, setup$T0)),
+    tolerance = tol
+  )
+})
+
+test_that("did point estimate agrees with the reference implementation", {
+  skip_if_not_installed("CVXR")
+  data(california_prop99)
+  setup <- panel.matrices(california_prop99)
+  expect_equal(
+    c(did_estimate(setup$Y, setup$N0, setup$T0)),
+    c(did.reference(setup$Y, setup$N0, setup$T0)),
+    tolerance = tol
+  )
+})
